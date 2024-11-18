2568a632179f8af9ca84dfd03c30634d1d9e5fc1
Fahad <fahad.alabdulkarim5@gmail.com>
2024-11-18 10:22:55 +0300

    FahadSite

--- FahadSite/FahadSite/settings.py
+++ FahadSite/FahadSite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-h=5_vjnnu8y!d#53rl&oru=l8ln56s(bus$xe^)b!^7uv%a))6
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://themestoredep-production.up.railway.app", "http://127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = ["https://FahadSite.up.railway.app", "http://127.0.0.1"]
 
 
 # Application definition
