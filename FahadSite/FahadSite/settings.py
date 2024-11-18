3c39c0644ef99800bea14773ed0ed3a4d47a06f9
Fahad <fahad.alabdulkarim5@gmail.com>
2024-11-18 10:48:15 +0300

    fahadsite

--- FahadSite/FahadSite/settings.py
+++ FahadSite/FahadSite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,20 +82,13 @@ WSGI_APPLICATION = 'FahadSite.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
-    }
-} if not DEBUG else {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default']= dj_database_url.parse("postgresql://fahadsite_db_user:ZqM9nUhUp2zWtyJJwM3URZMJqRdcMopZ@dpg-cstetfl6l47c73ek65dg-a.oregon-postgres.render.com/fahadsite_db")
 
 
 # Password validation
